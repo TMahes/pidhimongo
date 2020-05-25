f9c4e2efc3f5a13a5d000f446c2a2686db6928aa
Maheswaran <t.mahesmca@gmail.com>
2020-05-25 17:02:05 +0530

    mail fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,4 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'freeindianshop3@gmail.com'
   layout 'mailer'
 end
-class UserMailer < ApplicationMailer
-end
-class ProfileMailer < ApplicationMailer
-end
+
